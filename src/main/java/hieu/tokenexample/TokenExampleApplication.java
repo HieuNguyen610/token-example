ba48c27423aef827e3e9ddf77db7629fa4c40403
hieunm2 <hieunm2@vnext.vn>
2025-04-16 14:59:11 +0700

    init

---
+++ src/main/java/hieu/tokenexample/TokenExampleApplication.java
@@ -0,0 +1,13 @@
+package hieu.tokenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/tokenexample/TokenExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.tokenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
